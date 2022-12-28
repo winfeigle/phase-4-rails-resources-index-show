5f9e8105297ca0304dd2782ad5123e1b9369c41d
Win Feigle <win.feigle@gmail.com>
2022-12-28 16:35:11 -0700

    commit tree e1b19d75c353b67d7aa5c0883c5738913f39acf3

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,17 @@
+class BirdsController < ApplicationController
+
+    # GET /birds
+    def index 
+        render json: Bird.all
+    end 
+
+    def show
+        bird = Bird.find(params[:id])
+        if bird
+            render json: bird
+        else
+            render json: { error: "Bird not found" }, status: :not_found
+        end
+    end
+
+end
